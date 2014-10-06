84d27219a343a0667e568d78157ea5f0e9e3ef24
Richard Solomou <rsolomou@gmail.com>
2014-10-06 22:10:15 +0100

    Added NFC reading functionality for the main activity.

--- app/src/main/java/com/richardsolomou/atmos/MainActivity.java
+++ app/src/main/java/com/richardsolomou/atmos/MainActivity.java
@@ -5,8 +5,37 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+// NFC reader dependencies
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.tech.IsoDep;
+import android.nfc.tech.MifareClassic;
+import android.nfc.tech.MifareUltralight;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NfcA;
+import android.nfc.tech.NfcB;
+import android.nfc.tech.NfcF;
+import android.nfc.tech.NfcV;
+import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
+
+    // List of NFC technologies.
+    private final String[][] techList = new String[][] {
+        new String[] {
+            NfcA.class.getName(),
+            NfcB.class.getName(),
+            NfcF.class.getName(),
+            NfcV.class.getName(),
+            IsoDep.class.getName(),
+            MifareClassic.class.getName(),
+            MifareUltralight.class.getName(),
+            Ndef.class.getName()
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +43,50 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Create pending intent.
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        // Create intent receiver for NFC events.
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(NfcAdapter.ACTION_TAG_DISCOVERED);
+        filter.addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filter.addAction(NfcAdapter.ACTION_TECH_DISCOVERED);
+        // Enable foreground dispatch for getting intent from NFC event.
+        NfcAdapter mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        mNfcAdapter.enableForegroundDispatch(this, pendingIntent, new IntentFilter[]{filter}, this.techList);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Disable foreground dispatch.
+        NfcAdapter mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        mNfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if (intent.getAction().equals(NfcAdapter.ACTION_TAG_DISCOVERED)) {
+            String tag = bytesToHex(intent.getByteArrayExtra(NfcAdapter.EXTRA_ID));
+            ((TextView)findViewById(R.id.text)).setText("NFC Tag: " + tag);
+        }
+    }
+
+    // Set the hexadecimal values in an array.
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    // Method to convert the byte array to hexadecimal.
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
