76db93d23d23b5b906a0396638578d7bdbdf558f
Richard Solomou <rsolomou@gmail.com>
2014-10-30 23:20:38 +0000

    Added a base activity to be called and extended by all activities.

---
+++ app/src/main/java/com/richardsolomou/atmos/BaseActivity.java
@@ -0,0 +1,91 @@
+package com.richardsolomou.atmos;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
+
+public abstract class BaseActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+	private static final int RC_SIGN_IN = 0;
+	protected GoogleApiClient mGoogleApiClient;
+	protected boolean mIntentInProgress;
+
+	protected boolean mSignInClicked;
+	protected ConnectionResult mConnectionResult;
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_signin);
+
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+				.addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this)
+				.addApi(Plus.API)
+				.addScope(Plus.SCOPE_PLUS_LOGIN)
+				.build();
+	}
+
+	protected void onStart() {
+		super.onStart();
+		mGoogleApiClient.connect();
+	}
+
+	protected void onStop() {
+		super.onStop();
+
+		if (mGoogleApiClient.isConnected()) {
+			mGoogleApiClient.disconnect();
+		}
+	}
+
+	public void onConnectionFailed(ConnectionResult result) {
+		if (!mIntentInProgress) {
+			mConnectionResult = result;
+
+			if (mSignInClicked) {
+				resolveSignInError();
+			}
+		}
+	}
+
+	protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+		if (requestCode == RC_SIGN_IN) {
+			if (responseCode != RESULT_OK) {
+				mSignInClicked = false;
+			}
+
+			mIntentInProgress = false;
+
+			if (!mGoogleApiClient.isConnecting()) {
+				mGoogleApiClient.connect();
+			}
+		}
+	}
+
+	public void onConnectionSuspended(int cause) {
+		mGoogleApiClient.connect();
+	}
+
+	protected void resolveSignInError() {
+		if (mConnectionResult.hasResolution()) {
+			try {
+				mIntentInProgress = true;
+				startIntentSenderForResult(mConnectionResult.getResolution().getIntentSender(), RC_SIGN_IN, null, 0, 0, 0);
+			} catch (IntentSender.SendIntentException e) {
+				mIntentInProgress = false;
+				mGoogleApiClient.connect();
+			}
+		}
+	}
+
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		mSignInClicked = false;
+	}
+
+}
