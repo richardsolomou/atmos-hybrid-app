190f173ea13545bd45a73ec26239a6fdd44eca47
Richard Solomou <rsolomou@gmail.com>
2015-01-21 01:26:29 +0000

    Fully documented MainActivity.

--- app/src/main/java/com/richardsolomou/atmos/MainActivity.java
+++ app/src/main/java/com/richardsolomou/atmos/MainActivity.java
@@ -25,17 +25,43 @@ import android.widget.Toast;
 import com.google.android.gms.plus.model.people.Person;
 import com.google.android.gms.plus.Plus;
 
-
+/**
+ * This class serves as the main activity for the application. It contains methods that allow it to
+ * talk to the device's hardware to listen for an RFID chip and send its UID to the web application.
+ *
+ * @author Richard Solomou <richard@richardsolomou.com>
+ * @version 0.2
+ * @since 0.1
+ */
 public class MainActivity extends BaseActivity {
 
-	String protocol = "http://";
-	String hostName = "www2.richardsolomou.com";
-	String homePage = "/atmos";
-	String objectName = "ATMOS";
-	WebView webView;
+	/**
+	 * The protocol being used for the server.
+	 */
+	private String protocol = "http://";
 
 	/**
-	 * NFC technologies.
+	 * The hostname used for the server.
+	 */
+	private String hostName = "www2.richardsolomou.com";
+
+	/**
+	 * The path of the web application.
+	 */
+	private String homePage = "/atmos";
+
+	/**
+	 * The name of this application.
+	 */
+	private String objectName = "ATMOS";
+
+	/**
+	 * The web view handler.
+	 */
+	private WebView webView;
+
+	/**
+	 * The supported NFC technologies.
 	 */
 	private final String[][] techList = new String[][]{
 			new String[]{
@@ -51,39 +77,62 @@ public class MainActivity extends BaseActivity {
 	};
 
 	/**
-	 * Set the hexadecimal values in an array.
+	 * All hexadecimal values in a character array.
 	 */
 	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
 
-	// Method to convert the byte array to hexadecimal.
-	public static String bytesToHex(byte[] bytes) {
+	/**
+	 * Converts the byte array received by the RFID chip to hexadecimal.
+	 *
+	 * @param bytes the byte array
+	 * @return the converted string
+	 */
+	private static String bytesToHex(byte[] bytes) {
 		char[] hexChars = new char[bytes.length * 2];
+
 		for (int j = 0; j < bytes.length; j++) {
 			int v = bytes[j] & 0xFF;
 			hexChars[j * 2] = hexArray[v >>> 4];
 			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
 		}
+
 		return new String(hexChars);
 	}
 
+	/**
+	 * Checks if there is a network connection available.
+	 *
+	 * @return true if there is a network connection, false otherwise
+	 */
 	private boolean isNetworkAvailable() {
 		ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
 		NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
 		return activeNetworkInfo != null && activeNetworkInfo.isConnected();
 	}
 
+	/**
+	 * Extends a WebView client and overrides on links outside the hostname.
+	 */
 	private class MyWebViewClient extends WebViewClient {
 		@Override
 		public boolean shouldOverrideUrlLoading(WebView view, String url) {
 			if (Uri.parse(url).getHost().equals(hostName)) {
 				return false;
 			}
+
 			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
 			startActivity(intent);
+
 			return true;
 		}
 	}
 
+	/**
+	 * Initialises the activity, configures the WebView client and directs the user to the web
+	 * application.
+	 *
+	 * @param savedInstanceState the saved instance state
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -104,11 +153,19 @@ public class MainActivity extends BaseActivity {
 		webView.loadUrl(protocol + hostName + homePage);
 		webView.addJavascriptInterface(new WebAppInterface(this), objectName);
 		webView.setWebViewClient(new MyWebViewClient());
+
 		/**
 		 * TODO: Send device data to JavaScript.
 		 */
 	}
 
+	/**
+	 * Sends the user back on the WebView or alternatively closes the application.
+	 *
+	 * @param keyCode the key code
+	 * @param event   the key event
+	 * @return true if the back key was pressed, false otherwise
+	 */
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		WebView myWebView = (WebView) findViewById(R.id.webview);
@@ -119,16 +176,27 @@ public class MainActivity extends BaseActivity {
 		return super.onKeyDown(keyCode, event);
 	}
 
+	/**
+	 * Sends the UID received by the RFID chip to the server-side.
+	 *
+	 * @param intent the intent
+	 */
 	@Override
 	protected void onNewIntent(Intent intent) {
 		if (intent.getAction().equals(NfcAdapter.ACTION_TAG_DISCOVERED)) {
 			String uid = bytesToHex(intent.getByteArrayExtra(NfcAdapter.EXTRA_ID));
+
 			/**
 			 * TODO: Send UID to JavaScript.
 			 */
 		}
 	}
 
+	/**
+	 * Sends the user data received from the SignInActivity to the server-side.
+	 *
+	 * @param connectionHint the connection hint
+	 */
 	public void onConnected(Bundle connectionHint) {
 		try {
 			if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
@@ -138,36 +206,39 @@ public class MainActivity extends BaseActivity {
 				String personProfile = currentPerson.getUrl();
 				String personEmail = Plus.AccountApi.getAccountName(mGoogleApiClient);
 				personPhoto = personPhoto.substring(0, personPhoto.length() - 2) + 150;
+
 				/**
 				 * TODO: Send user data to JavaScript.
 				 */
 			} else {
-				Toast.makeText(getApplicationContext(), "Person information is null", Toast.LENGTH_LONG).show();
+				Toast.makeText(getApplicationContext(), "Person information is empty", Toast.LENGTH_LONG).show();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * Creates the intent for NFC events and enables foreground dispatch for capturing intents.
+	 */
 	@Override
 	protected void onResume() {
 		super.onResume();
-		// Create pending intent.
 		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-		// Create intent receiver for NFC events.
 		IntentFilter filter = new IntentFilter();
 		filter.addAction(NfcAdapter.ACTION_TAG_DISCOVERED);
 		filter.addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
 		filter.addAction(NfcAdapter.ACTION_TECH_DISCOVERED);
-		// Enable foreground dispatch for getting intent from NFC event.
 		NfcAdapter mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 		mNfcAdapter.enableForegroundDispatch(this, pendingIntent, new IntentFilter[]{filter}, techList);
 	}
 
+	/**
+	 * Disables foreground dispatch for capturing intents.
+	 */
 	@Override
 	protected void onPause() {
 		super.onPause();
-		// Disable foreground dispatch.
 		NfcAdapter mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 		mNfcAdapter.disableForegroundDispatch(this);
 	}
