945c8d74ad040e5a91a9a35177db074938d742cb
Richard Solomou <rsolomou@gmail.com>
2015-01-21 00:33:47 +0000

    Fully documented SignInActivity.

--- app/src/main/java/com/richardsolomou/atmos/SignInActivity.java
+++ app/src/main/java/com/richardsolomou/atmos/SignInActivity.java
@@ -5,9 +5,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-
+/**
+ * This class employs Google Play Services to sign in the user with their Google account.
+ *
+ * @author Richard Solomou <richard@richardsolomou.com>
+ * @version 0.1
+ * @since 0.1
+ */
 public class SignInActivity extends BaseActivity implements View.OnClickListener {
 
+	/**
+	 * Initialises the activity and sets a click listener on the sign in button.
+	 *
+	 * @param savedInstanceState the saved instance state
+	 */
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_signin);
@@ -15,6 +26,11 @@ public class SignInActivity extends BaseActivity implements View.OnClickListener
 		findViewById(R.id.btnSignIn).setOnClickListener(this);
 	}
 
+	/**
+	 * Attempts to sign in the user with Google Play Services.
+	 *
+	 * @param view the view
+	 */
 	public void onClick(View view) {
 		if (view.getId() == R.id.btnSignIn && !mGoogleApiClient.isConnecting()) {
 			mSignInClicked = true;
@@ -22,6 +38,11 @@ public class SignInActivity extends BaseActivity implements View.OnClickListener
 		}
 	}
 
+	/**
+	 * Starts the main activity and ends the current one.
+	 *
+	 * @param connectionHint the connection hint
+	 */
 	public void onConnected(Bundle connectionHint) {
 		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
 		Intent intent = new Intent(getApplicationContext(), MainActivity.class);
