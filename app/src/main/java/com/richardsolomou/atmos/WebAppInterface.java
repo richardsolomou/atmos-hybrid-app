1a1830b8624b7608649ab260d232ad9b035ab4d3
Richard Solomou <rsolomou@gmail.com>
2015-01-21 00:38:54 +0000

    Fully documented WebAppInterface.

--- app/src/main/java/com/richardsolomou/atmos/WebAppInterface.java
+++ app/src/main/java/com/richardsolomou/atmos/WebAppInterface.java
@@ -4,16 +4,37 @@ import android.content.Context;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
 
+/**
+ * This class creates an interface for the web application and contains functions that can be called
+ * through JavaScript on the server-side.
+ *
+ * @author Richard Solomou <richard@richardsolomou.com>
+ * @version 0.2
+ * @since 0.2
+ */
 public class WebAppInterface {
 
+	/**
+	 * Sets the context variable.
+	 */
 	Context mContext;
 
+	/**
+	 * Instantiates a new web application interface.
+	 *
+	 * @param c the context parameter
+	 */
 	WebAppInterface(Context c) {
 		mContext = c;
 	}
 
+	/**
+	 * Shows a toast notification to the user.
+	 *
+	 * @param str notification string
+	 */
 	@JavascriptInterface
-	public void showToast(String toast) {
-		Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+	public void showToast(String str) {
+		Toast.makeText(mContext, str, Toast.LENGTH_SHORT).show();
 	}
 }
